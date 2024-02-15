553b4cdb369e38e17c8070a29d2d9006ebee3d88
ahmadjon2008 <robotoprotecno2008@gmail.com>
2024-02-15 10:06:16 +0500

    Decorator commit

--- innerless.py
+++ innerless.py
@@ -1,7 +1,7 @@
 def my_deca(func):
-    def inner():
+    def inner(a):
         print("Decorator work")
-        func()
+        func(a)
         print("Decorator stopped")
 
     return inner
@@ -13,11 +13,22 @@ def my_deca(func):
 # a = my_deca(my_func)
 # a()
 
+# @my_deca
+# def my_func2():
+#     a = []
+#     for i in range(5000000 + 1):
+#         a.append(i)
+#
+#
+# my_func2()
+
+
 @my_deca
-def my_func2():
-    a = []
-    for i in range(5000000 + 1):
-        a.append(i)
+def my_func3(a: int):
+    if a % 2:
+        print("Toq")
+    else:
+        print("Juft")
 
 
-my_func2()
+my_func3(5)
